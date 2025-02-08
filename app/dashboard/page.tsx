f8944d08eb328573b8bab79f103f3880ae6b63ee
fangyi <fangyifangyif@126.com>
2025-02-08 20:39:24 +0800

    feat:  fetch data in server

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,37 @@
-export default function Page() {
+import { Card } from '@/app/ui/dashboard/cards';
+import RevenueChart from '@/app/ui/dashboard/revenue-chart';
+import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+import { lusitana } from '@/app/ui/fonts';
+// 获取数据的函数
+import { fetchRevenue, fetchLatestInvoices, fetchCardData } from '@/app/lib/data';
+ 
+export default async function Page() {
+  // 收入
+  const revenue = await fetchRevenue();
+  // 发票
+  const latestInvoices = await fetchLatestInvoices();
+
+  // 卡片数据
+  const { totalPaidInvoices, totalPendingInvoices, numberOfInvoices, numberOfCustomers} = await fetchCardData();
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1 className="text-4xl font-bold">Home Page</h1>
-      <p className="mt-4 text-lg">Welcome to your dashboard home page!</p>
-      {/* Add your dashboard content here */}
-    </div>
+    <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        <Card title="Collected" value={totalPaidInvoices} type="collected" />
+        <Card title="Pending" value={totalPendingInvoices} type="pending" />
+        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+        <Card
+          title="Total Customers"
+          value={numberOfCustomers}
+          type="customers"
+        />
+      </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <RevenueChart revenue={revenue}  />
+        <LatestInvoices latestInvoices={latestInvoices} />
+      </div>
+    </main>
   );
 }
